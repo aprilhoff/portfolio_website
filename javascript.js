77e43b9a8d7f9dbad6ff3cfb84ae26a31e49a941
aprilhoff <hoffa617@newschool.edu>
2026-01-09 14:57:03 -0500

    newfiles

---
+++ javascript.js
@@ -0,0 +1,75 @@
+// UNSENT & UNREAD
+let unsentIndex = 1;
+showUnsent(unsentIndex);
+
+function showUnsent(n) {
+  let slides = document.getElementsByClassName("mySlides-unsent");
+  if (!slides.length) return;
+
+  if (n > slides.length) unsentIndex = 1;
+  if (n < 1) unsentIndex = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[unsentIndex - 1].style.display = "block";
+}
+
+for (let slide of document.getElementsByClassName("mySlides-unsent")) {
+  slide.addEventListener("click", () => {
+    unsentIndex++;
+    showUnsent(unsentIndex);
+  });
+}
+
+
+// BETWEEN HAND & MACHINE
+let aiIndex = 1;
+showAI(aiIndex);
+
+function showAI(n) {
+  let slides = document.getElementsByClassName("mySlides-ai");
+  if (!slides.length) return;
+
+  if (n > slides.length) aiIndex = 1;
+  if (n < 1) aiIndex = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[aiIndex - 1].style.display = "block";
+}
+
+for (let slide of document.getElementsByClassName("mySlides-ai")) {
+  slide.addEventListener("click", () => {
+    aiIndex++;
+    showAI(aiIndex);
+  });
+}
+
+// CORE03
+
+let core03Index = 1;
+showCore03(core03Index);
+
+function showCore03(n) {
+  let slides = document.getElementsByClassName("mySlides-core03");
+  if (!slides.length) return;
+
+  if (n > slides.length) core03Index = 1;
+  if (n < 1) core03Index = slides.length;
+
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[core03Index - 1].style.display = "block";
+}
+
+for (let slide of document.getElementsByClassName("mySlides-core03")) {
+  slide.addEventListener("click", () => {
+    core03Index++;
+    showCore03(core03Index);
+  });
+}
+
+
